8790cc858e7e0bbf2e46ad8a3811e70f25965f5e
CODENAMESOL3NYA <dultstersmusic@gmail.com>
2025-08-15 21:07:46 +0200

    add: compression, rate limiter and helmet

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require("dotenv");
+const helmet = require("helmet");
+const RateLimit = require("express-rate-limit");
+const compression = require("compression");
 const connectDB = require("./database/config");
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -20,8 +24,7 @@ if (process.env.NODE_ENV !== "production") {
 
 const app = express();
 
-//Connect Database
-connectDB();
+app.set('trust proxy', 1);
 
 if (process.env.NODE_ENV === "production") {
   app.use(logger("combined"));
@@ -29,7 +32,19 @@ if (process.env.NODE_ENV === "production") {
   app.use(logger("dev"));
 }
 
+const limiter = RateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
+//Connect Database
+connectDB();
+
+app.use(helmet())
+app.use(compression());
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -38,6 +53,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/goals',goalsRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 
 module.exports = app;
